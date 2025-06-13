8064210e1e6931848743a71fa9f0059dc3648c0f
sunnywebsite <malikars582@gmail.com>
2025-06-13 16:39:31 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
-  <HashRouter>
-    <React>
+  <BrowserRouter>
+    <React.StrictMode>
       <App />
-    </React>
-  </HashRouter>,
+    </React.StrictMode>
+  </BrowserRouter>,
   document.getElementById("root")
 );
 
